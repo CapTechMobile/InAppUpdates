ae2e5211d6e1a735fa42ad85702de2db4a18d841
Alisher Pazylbekov <apazylbekov@mbp-apazylbekov.local>
2019-05-18 16:57:40 -0400

    adding final changes for updating in flexible flow

--- app/src/main/java/com/captech/inappupdates/settings/MoreFragment.java
+++ app/src/main/java/com/captech/inappupdates/settings/MoreFragment.java
@@ -2,15 +2,19 @@ package com.captech.inappupdates.settings;
 
 import android.content.Intent;
 import android.content.IntentSender;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.captech.inappupdates.MainActivity;
 import com.captech.inappupdates.R;
@@ -31,17 +35,29 @@ public class MoreFragment extends Fragment implements InstallStateUpdatedListene
     AppUpdateManager appUpdateManager;
 
     public static String FLEXIBLE_UPDATE = "flexible_update";
-
+    AppUpdateInfo appUpdateInfo;
+    TextView updateTextView;
+    Button updateButton;
+    
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.more_main, container, false);
-
         Bundle bundle = getArguments();
         boolean needsFlexibleUpdate = bundle.getBoolean(FLEXIBLE_UPDATE);
 
         LinearLayout flexibleUpdateLayout = view.findViewById(R.id.updateContainer);
         flexibleUpdateLayout.setVisibility(needsFlexibleUpdate ? View.VISIBLE : View.GONE);
-
+        
+        updateTextView = view.findViewById(R.id.updateView);
+        updateButton = view.findViewById(R.id.updateButton);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (appUpdateInfo != null) {
+                    startUpdate(appUpdateInfo, AppUpdateType.FLEXIBLE);
+                }
+            }
+        });
         return view;
     }
 
@@ -74,35 +90,33 @@ public class MoreFragment extends Fragment implements InstallStateUpdatedListene
         int installStatus = installState.installStatus();
         switch (installStatus) {
             case InstallStatus.DOWNLOADING:
-                //show downloading UI
-                break;
-            case InstallStatus.INSTALLING:
-                //show downloading UI
+                updateTextView.setText(R.string.downloading_text);
+                updateButton.setVisibility(View.INVISIBLE);
                 break;
             case InstallStatus.DOWNLOADED:
-                //show downloading UI
-                break;
-            case InstallStatus.INSTALLED:
-                //show downloading UI
+                updateTextView.setText(R.string.downloaded_text);
+                updateButton.setVisibility(View.INVISIBLE);
+                popupSnackbarForCompleteUpdate();
                 break;
             case InstallStatus.FAILED:
-                //show downloading UI
+                updateTextView.setText(R.string.download_failed_text);
                 break;
             case InstallStatus.CANCELED:
-                //show downloading UI
-                break;
-            case InstallStatus.PENDING:
-                //show pending UI
+                updateTextView.setText(R.string.download_canceled_text);
                 break;
         }
     }
 
     @Override
     public void onSuccess(AppUpdateInfo appUpdateInfo) {
-        if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+        if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+            // If the update is downloaded but not installed,
+            // notify the user to complete the update.
+            popupSnackbarForCompleteUpdate();
+        } else if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
                 && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
             //show flexible update available UI and on response from that, start flexible update
-            startUpdate(appUpdateInfo, AppUpdateType.FLEXIBLE);
+            this.appUpdateInfo = appUpdateInfo;
         }
     }
 
@@ -115,5 +129,22 @@ public class MoreFragment extends Fragment implements InstallStateUpdatedListene
         } catch (IntentSender.SendIntentException e) {
             e.printStackTrace();
         }
+    }
+
+    /* Displays the snackbar notification and call to action. */
+    private void popupSnackbarForCompleteUpdate() {
+        Snackbar snackbar =
+                Snackbar.make(
+                        requireActivity().findViewById(R.id.main_activity),
+                        "An update has just been downloaded.",
+                        Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction("RESTART", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                appUpdateManager.completeUpdate();
+            }
+        });
+        snackbar.setActionTextColor(Color.RED);
+        snackbar.show();
     }
 }
