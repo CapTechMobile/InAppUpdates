20822b31159ac60334619b7f5daf67b02c8e8976
Alisher Pazylbekov <apazylbekov@mbp-apazylbekov.local>
2019-05-18 11:29:57 -0400

    adding in in app update functionality

--- app/src/main/java/com/captech/inappupdates/MainActivity.java
+++ app/src/main/java/com/captech/inappupdates/MainActivity.java
@@ -1,21 +1,38 @@
 package com.captech.inappupdates;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageInfo;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.OnSuccessListener;
+
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnSuccessListener<AppUpdateInfo> {
 
     private TextView mVersionNumber;
     private RecyclerView mEmployeeList;
+    private AppUpdateManager appUpdateManager;
+    private static final int REQUEST_CODE = 123456789;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +45,18 @@ public class MainActivity extends AppCompatActivity {
 
         setEmployeeAdapter();
         setVersionText();
+    }
+
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+        appUpdateManager = AppUpdateManagerFactory.create(context);
+        return super.onCreateView(parent, name, context, attrs);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        appUpdateManager.getAppUpdateInfo().addOnSuccessListener(this);
     }
 
     @Override
@@ -55,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
     private void setVersionText() {
         PackageInfo packageInfo = null;
         try {
-             packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
         } catch (Exception e) {
 
         }
@@ -63,7 +92,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setEmployeeAdapter() {
-//        ArrayList<Employee> employeeList = Employee.createEmployeeList(2);
         ArrayList<Employee> employeeList = new ArrayList<>();
         employeeList.add(new Employee("Alisher Pazylbekov", R.drawable.profilepicture1));
         employeeList.add(new Employee("JP Garduno", R.drawable.profilepicture2));
@@ -71,6 +99,44 @@ public class MainActivity extends AppCompatActivity {
         EmployeeAdapter adapter = new EmployeeAdapter(employeeList);
 
         mEmployeeList.setAdapter(adapter);
-        mEmployeeList.setLayoutManager(new LinearLayoutManager( this));
+        mEmployeeList.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public void onSuccess(AppUpdateInfo appUpdateInfo) {
+        if (appUpdateInfo.updateAvailability()
+                == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+            // If an in-app update is already running, resume the update.
+            try {
+                appUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        AppUpdateType.IMMEDIATE,
+                        this,
+                        REQUEST_CODE);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+            try {
+                appUpdateManager.startUpdateFlowForResult(appUpdateInfo,
+                        AppUpdateType.IMMEDIATE,
+                        this,
+                        REQUEST_CODE);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Log.i(MainActivity.class.getSimpleName(), "Update flow completed! Result code: " + resultCode);
+            } else {
+                Log.e(MainActivity.class.getSimpleName(), "Update flow failed! Result code: " + resultCode);
+            }
+        }
     }
 }
