4feaed1c5443f75174b5078baccdf9ae60c8f4e7
Alisher Pazylbekov <apazylbekov@mbp-apazylbekov.local>
2019-05-19 13:13:23 -0400

    unused imports

--- app/src/main/java/com/captech/inappupdates/MainActivity.java
+++ app/src/main/java/com/captech/inappupdates/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageInfo;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
